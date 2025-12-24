eb9f7b2383112141aea8523993b5fbd25ade307a
sjnhnp <18591921+sjnhnp@users.noreply.github.com>
2025-12-24 15:22:38 +0800

    版本号

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -21,7 +21,7 @@ import (
 )
 
 // Version can be set at build time via -ldflags "-X 'guiforcores/bridge.Version=vX.Y.Z'"
-var Version = "v1.16.0"
+var Version = "v1.18.0"
 
 var Config = &AppConfig{}
 
